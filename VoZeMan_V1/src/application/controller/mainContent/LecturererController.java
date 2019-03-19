e178f968260ae760531a85bf2c19066a7f542281
Nicolas Neuhof <wildpark11@t-online.de>
2019-03-19 13:54:51 +0100

    .

--- VoZeMan_V1/src/application/controller/mainContent/LecturererController.java
+++ VoZeMan_V1/src/application/controller/mainContent/LecturererController.java
@@ -1,6 +1,38 @@
 package application.controller.mainContent;
 
-public class LecturererController  {
+import java.awt.TextArea;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class LecturererController implements Initializable {
+	
+	@FXML
+	private GridPane gridPane;
+	
+	@FXML
+	private Label selectLecturerLabel;
+	
+	@FXML
+	private ComboBox<String> selectLecturerComboBox;
+	
+	@FXML
+	private TextArea showLecturerTextArea;
+	
+	
+	
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
